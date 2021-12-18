76e38a1eb78375b7d63f7447edd236659dcea046
AareuSoftnet <aareusoftnet@gmail.com>
2021-12-18 21:28:29 +0530

    - ADDED new portfolio details

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  bracketSpacing: false,
+  bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 100,
 };
